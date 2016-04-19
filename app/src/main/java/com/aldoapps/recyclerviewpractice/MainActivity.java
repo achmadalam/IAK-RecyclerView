4d9963a382c4552eb7c27d94e80caa1d8710853f
Aldo Kelvianto <aldokelvianto@yahoo.com>
2016-04-19 17:25:14 +0700

    beautify list view

--- app/src/main/java/com/aldoapps/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/aldoapps/recyclerviewpractice/MainActivity.java
@@ -1,13 +1,10 @@
 package com.aldoapps.recyclerviewpractice;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -34,25 +31,20 @@ public class MainActivity extends AppCompatActivity
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // untuk nambahin garis pembatas di recycler view
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
         mRecyclerView.setAdapter(mAdapter);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
     public void OnCatClicked(Cat cat) {
+        String message = cat.getName() + " says hello!";
         Toast.makeText(MainActivity.this,
-                "cat name " + cat.getName(),
+                message,
                 Toast.LENGTH_SHORT).show();
     }
 }
